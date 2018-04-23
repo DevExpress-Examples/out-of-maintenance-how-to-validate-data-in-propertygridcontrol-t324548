9999bb235fee77f01c8e770dd93190b57b2c525a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.ComponentModel;
+using DevExpress.Xpf.PropertyGrid;
+
+namespace DXSample {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void PropertyGridControl_ValidateCell(object sender, ValidateCellEventArgs e) {
+            if (e.NewValue == null || string.IsNullOrEmpty(e.NewValue.ToString()))
+                e.ValidationException = new System.Exception("A cell cannot be empty");
+        }
+    }
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows
+Imports System.Collections.ObjectModel
+Imports System.Globalization
+Imports System.Windows.Data
+Imports System.Windows.Markup
+Imports System.Windows.Controls
+Imports System.Windows.Media
+Imports System.ComponentModel
+Imports DevExpress.Xpf.PropertyGrid
+
+Namespace DXSample
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+        Private Sub PropertyGridControl_ValidateCell(ByVal sender As Object, ByVal e As ValidateCellEventArgs)
+            If e.NewValue Is Nothing OrElse String.IsNullOrEmpty(e.NewValue.ToString()) Then
+                e.ValidationException = New System.Exception("A cell cannot be empty")
+            End If
+        End Sub
+    End Class
+End Namespace
